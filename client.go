8e88a8621eb9f74c8b2e56bf842a70e7218a5362
lucklrj <sunny_lrj@yeah.net>
2021-01-24 21:09:58 +0800

    增加包

--- client.go
+++ client.go
@@ -1,7 +1,23 @@
 package baidu_map_sdk
 
-import "fmt"
+import (
+	"github.com/ddliu/go-httpclient"
+)
 
-func New(ak string){
-	fmt.Println(ak)
+type Client struct {
+	ak string
+	HttpClient *httpclient.HttpClient
+	isNeedSn bool
+}
+func New(ak string,isNeedSn bool) *Client{
+	httpClient := httpclient.NewHttpClient().Defaults(httpclient.Map{
+		httpclient.OPT_USERAGENT: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:64. 0) Gecko/20100101 Firefox/64.0",
+		//httpclient.OPT_USERAGENT:  "Mozilla/5.0 (Windows NT 6.1; rv:24.0) Gecko/20100101 Firefox/24.0",
+		httpclient.OPT_UNSAFE_TLS: true,
+	})
+	return &Client{
+		ak:ak,
+		HttpClient: httpClient,
+		isNeedSn:isNeedSn,
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/lucklrj/baidu-map-sdk
 
 go 1.13
+
+require github.com/ddliu/go-httpclient v0.6.9
